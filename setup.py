605df9760a405d2d1f6299999e3ad441e4699c66
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-16 18:13:29 +0200

    Prepare for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from distutils.core import setup
 
-setup(name='sexpdata',
-      version='0.0.0',
-      description='S-expression parser for Python',
-      author='Takafumi Arakaki',
-      py_modules=['sexpdata'],
-      )
+import sexpdata
+
+setup(
+    name='sexpdata',
+    version=sexpdata.__version__,
+    py_modules=['sexpdata'],
+    author=sexpdata.__author__,
+    author_email='aka.tkf@gmail.com',
+    url='https://github.com/tkf/sexpdata',
+    license=sexpdata.__license__,
+    description='S-expression parser for Python',
+    long_description=sexpdata.__doc__,
+    keywords='s-expression, lisp, parser',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Lisp",
+        "Programming Language :: Emacs-Lisp",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+)
