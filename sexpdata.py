4272396155ed1fb2ba4b239485b0fed14897f2b6
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-16 18:48:04 +0200

    Add links in docstring

--- sexpdata.py
+++ sexpdata.py
@@ -1,6 +1,15 @@
 """
 S-expression parser for Python
 ==============================
+
+Links:
+
+* `Documentaions (at Read the Docs) <http://sexpdata.readthedocs.org/>`_
+* `Repository (at GitHub) <https://github.com/tkf/sexpdata>`_
+* `Issue tracker (at GitHub) <https://github.com/tkf/sexpdata/issues>`_
+* `PyPI <pypi.python.org/pypi/sexpdata>`_
+* `Travis CI <https://travis-ci.org/#!/tkf/sexpdata>`_
+
 """
 
 # Copyright (c) 2012 Takafumi Arakaki
