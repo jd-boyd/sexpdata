45f597a23be869be4fc3b625bc587a641032c480
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-18 22:06:00 +0100

    Bump to 0.0.2.dev0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,9 +26,9 @@ project = u'sexpdata'
 copyright = u'2012, Takafumi Arakaki'
 
 # The short X.Y version.
-version = '0.0.1'
+version = '0.0.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.1'
+release = '0.0.2.dev0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
