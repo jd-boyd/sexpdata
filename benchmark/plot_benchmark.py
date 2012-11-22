0c57fdf03e021a5f709b2b09b68de17e65032780
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-22 03:15:29 +0100

    Generate HTML in benchmark/plot_benchmark.py

--- benchmark/plot_benchmark.py
+++ benchmark/plot_benchmark.py
@@ -6,16 +6,38 @@ from run_benchmark import benchmarks, REPO_PATH, DB_PATH
 
 PLOT_DIR = os.path.join(REPO_PATH, 'tmp', 'plot')
 
+html_template = """
+<html>
+<body>
+{0}
+</body>
+</html>
+"""
+
+img_template = """
+<img style="width-max: 100%;" src="{0}">
+"""
+
+
+def generate_html(fignames):
+    return html_template.format("\n".join(map(img_template.format, fignames)))
+
 
 def main():
     if not os.path.isdir(PLOT_DIR):
         os.makedirs(PLOT_DIR)
 
+    figname_list = []
     for bm in benchmarks:
         fig = pyplot.figure()
         bm.plot(DB_PATH, ax=pyplot.gca())
-        figpath = os.path.join(PLOT_DIR, '{0}.png'.format(bm.name))
+        figname = '{0}.png'.format(bm.name)
+        figpath = os.path.join(PLOT_DIR, figname)
         fig.savefig(figpath, bbox_inches='tight')
+        figname_list.append(figname)
+
+    with open(os.path.join(PLOT_DIR, 'index.html'), 'w') as f:
+        f.write(generate_html(figname_list))
 
 
 if __name__ == '__main__':
