d2ed22a9d92f7260a911d0df151755a8669f34dd
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-22 03:47:41 +0100

    Move benchmark data to benchcode.py

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -3,44 +3,13 @@ from datetime import datetime
 
 from vbench.api import Benchmark, BenchmarkRunner
 
+import benchcode
+
+
 START_DATE = datetime(2012, 10, 16)
 
-common_setup = """
-import sexpdata
-length = 10000
-"""
-do_loads = 'sexpdata.loads(string)'
-
 benchmarks = [
-    Benchmark(
-        do_loads,
-        common_setup + r"""
-string = '"{0}"'.format('x' * length)
-""",
-        name="Plain long string (plain:quote = 1:0)",
-        start_date=START_DATE),
-    Benchmark(
-        do_loads,
-        common_setup + r"""
-string = '"{0}"'.format(r'\"' * length)
-""",
-        name="Long string only with escaped quotes (plain:quote = 0:1)",
-        start_date=START_DATE),
-    Benchmark(
-        do_loads,
-        common_setup + r"""
-string = '"{0}"'.format(r'1\"' * length)
-""",
-        name="Long mixed string (plain:quote = 1:1)",
-        start_date=START_DATE),
-    Benchmark(
-        do_loads,
-        common_setup + r"""
-string = '"{0}"'.format(r'12345\"' * length)
-""",
-        name="Long mixed string (plain:quote = 5:1)",
-        start_date=START_DATE),
-]
+    Benchmark(start_date=START_DATE, **kwds) for kwds in benchcode.data]
 
 bench_path = os.path.dirname(os.path.abspath(__file__))
 REPO_PATH = os.path.dirname(bench_path)
