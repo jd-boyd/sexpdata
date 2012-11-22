05f5da23a56a650e3ba9d119e139d48ab789e5a3
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-22 03:55:32 +0100

    Add benchmark/run_timeit.py

---
+++ benchmark/run_timeit.py
@@ -0,0 +1,26 @@
+import timeit
+
+import benchcode
+
+
+def do_timeit(code, setup, name, number):
+    print(name)
+    sec = timeit.timeit(code, setup, number=number) / number
+    print("{0:.3f} sec".format(sec))
+
+
+def run_timeit(number):
+    for data in benchcode.data:
+        do_timeit(number=number, **data)
+
+
+def main(args=None):
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--number', type=int, default=10)
+    ns = parser.parse_args(args)
+    run_timeit(**vars(ns))
+
+
+if __name__ == '__main__':
+    main()
