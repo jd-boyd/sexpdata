46f01613d7e17526b83decc0fadfa6407fe41878
Joshua D. Boyd <jdboyd@jdboyd.net>
2023-06-06 13:39:16 -0400

    Bump version for release soon, update to modern packaging.
    Cleanup classifiers as well.

--- sexpdata.py
+++ sexpdata.py
@@ -64,7 +64,7 @@ See the source code for more information.
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '1.0.0'
+__version__ = '0.0.5'
 __author__ = 'Joshua D. Boyd, Takafumi Arakaki'
 __license__ = 'BSD License'
 __all__ = [
@@ -498,6 +498,7 @@ class Symbol(String):
     ]
 
     _lisp_quoted_to_raw = dict((q, r) for (r, q) in _lisp_quoted_specials)
+
 
 @tosexp.register(Symbol)
 def _(obj, **kwds):
